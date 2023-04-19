8748a7eb893b801d42de5710734b4b0dd7620bd8
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2023-04-19 17:11:14 -0500

    update

---
+++ js/app.js
@@ -0,0 +1,23 @@
+(function($, document, window){
+	
+	$(document).ready(function(){
+
+		// Cloning main navigation for mobile menu
+		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+		// Mobile menu toggle 
+		$(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
+
+		$(".hero").flexslider({
+			directionNav: false,
+			controlNav: true
+		});
+	});
+
+	$(window).load(function(){
+
+	});
+
+})(jQuery, document, window);
---
+++ js/min/app-min.js
@@ -0,0 +1,1 @@
+!function($,n,i){$(n).ready(function(){$(".mobile-navigation").append($(".main-navigation .menu").clone()),$(".menu-toggle").click(function(){$(".mobile-navigation").slideToggle()}),$(".hero").flexslider({directionNav:!1,controlNav:!0})}),$(i).load(function(){})}(jQuery,document,window);
