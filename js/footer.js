16153664f5edd2fe0ebde3ec88015ab37dac590e
Kenyon Bowers <KenyonBowersJehovah@gmail.com>
2025-01-01 10:59:51 -0500

    Major website revision
    I made a major revision of the website. Started from scratch and made it over again.

---
+++ js/footer.js
@@ -0,0 +1,18 @@
+document.write(`
+    <div class="bg-[#363641] py-4 text-[#acacb1] xl:pl-80 lg:pl-52 px-8 py-10">
+        <div class="flex md:flex-row flex-col gap-5">
+            <div class="container md:max-w-72">
+                <h3 class="text-[#dcc2a0] text-lg font-medium uppercase mb-2">About:</h3>
+                <p>I am Kenyon Bowers, I am an Independent Fundamental Baptist. I am a software developer and a preacher of God's Word.</p>
+                <hr class="my-2 border-b-[1px] border-[#777]">
+                <p>"And he said unto them, Go ye into all the world, and preach the gospel to every creature." (Mark 16:15)</p>
+            </div>
+            <div class="container md:max-w-72">
+                <h3 class="text-[#dcc2a0] text-lg font-medium uppercase mb-2">Contact:</h3>
+                <p><i class="fa fa-envelope"></i> <a href="mailto:KenyonBowersJehovah@gmail.com">KenyonBowersJehovah@gmail.com</a></p>
+                <p><i class="fa-brands fa-discord"></i> @kenyonbowers</p>
+            </div>
+        </div>
+        <p class="mt-4">Copyright &copy; 2025 Kenyon Bowers. All rights reserved.</p>
+    </div>
+`);
--- js/min/app-min.js
+++
@@ -1,1 +0,0 @@
-!function($,n,i){$(n).ready(function(){$(".mobile-navigation").append($(".main-navigation .menu").clone()),$(".menu-toggle").click(function(){$(".mobile-navigation").slideToggle()}),$(".hero").flexslider({directionNav:!1,controlNav:!0})}),$(i).load(function(){})}(jQuery,document,window);
